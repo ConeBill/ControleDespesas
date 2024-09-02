abf4085b804d0b0c0fe1b39103aa9ff29347a6fa
Paulo Henrique Vicente dos Santos Vieira <paulohvsv@gmail.com>
2024-09-02 08:02:05 -0300

    UpdadeDespesas
    Adicionada a rota para atualizar uma despesa.

--- routes/despesas.js
+++ routes/despesas.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { lerDadosDoArquivo, escreverDadosNoArquivo } = require('../utils/fileHandler');
-const Despesa = require('../models/despesaModel');
+const Despesa = require('../model/despesaModel');
 
 // Endpoint para obter todas as despesas
 router.get('/', (req, res) => {
@@ -26,6 +26,27 @@ router.post('/', (req, res) => {
     escreverDadosNoArquivo(despesas);
 
     res.status(201).json(despesa);
+ 
+});
+
+// Endpoint para atualizar uma despesa
+router.put('/:nome', (req, res) => {
+/*const { nome } = req.params;
+const updates = req.body;
+
+const despesas = lerDadosDoArquivo();
+const despesaIndex = despesas.findIndex(d => d.nome === nome);
+
+if (despesaIndex === -1) {
+    return res.status(404).json({ error: 'Despesa não encontrada' });
+}
+
+const despesaAtualizada = { ...despesas[despesaIndex], ...updates };
+despesas[despesaIndex] = despesaAtualizada;
+escreverDadosNoArquivo(despesas);
+
+res.json(despesaAtualizada);*/
+res.json({mgs: "Atualizando"})
 });
 
 module.exports = router;
