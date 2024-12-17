18de9af5299ea58cace3de138ca8c77bc8a0b860
Paulo Henrique Vicente dos Santos Vieira <paulohvsv@gmail.com>
2024-12-17 08:44:45 -0300

    att
    att

--- routes/contas.js
+++ routes/contas.js
@@ -4,6 +4,7 @@ const { QueryTypes } = require('sequelize');
 const sequelize = require('../config/database');
 const UsuarioConta = require('../model/usuarioContaModel');
 const { ContaBancaria } = require('../model/index');
+const { ca } = require('date-fns/locale');
 
 router.get('/usr', async (req, res) => {
     try {
@@ -43,98 +44,94 @@ router.post('/novaconta', async (req, res) => {
     if (!IdOrigem) {
         return res.status(400).json({ error: 'O campo IdOrigem é obrigatório' });
     }
-    if (!Nota) {
-        return res.status(400).json({ error: 'O campo Nota é obrigatório' });
-    }
-    if (!Saldo) {
-        return res.status(400).json({ error: 'O campo Saldo é obrigatório' });
-    }
+
 
     try {
         if (IdConta) {
-            const conta = await ContaBancaria.findOne({ where: { IdConta } });
-            if (!conta) {
-                return res.status(404).json({ error: 'Conta não encontrada' });
-            }
-
-            const usuarioConta = await UsuarioConta.findOne({
-                where: {
-                    IdConta,
-                    IdOrigem
+                const conta = await ContaBancaria.findOne({ where: { IdConta } });
+                if (!conta) {
+                    return res.status(404).json({ error: 'Conta não encontrada' });
                 }
-            });
 
-            if (!usuarioConta) {
-                if (Saldo > 0) {
-                    // Cria um saldo para o usuário e a conta
+                const usuarioConta = await UsuarioConta.findOne({
+                    where: {
+                        IdConta,
+                        IdOrigem
+                    }
+                });
+
+                if (!usuarioConta) {
+                    if (Saldo > 0) {
+
+                        // Cria um saldo para o usuário e a conta
+                        await UsuarioConta.create({
+                            IdConta: conta.IdConta,
+                            IdOrigem,
+                            SetorOrigem: 'Usuarios',
+                            saldo: Saldo
+                        });
+
+                        return res.status(201).json({ message: 'Conta criada e saldo associado ao usuário com sucesso.' });
+                    };
+                    // Cria o saldo para o usuário e a conta
                     await UsuarioConta.create({
                         IdConta: conta.IdConta,
                         IdOrigem,
                         SetorOrigem: 'Usuarios',
-                        saldo: Saldo
+                        saldo: 0.00
                     });
+                }
 
-                    return res.status(201).json({ message: 'Conta criada e saldo associado ao usuário com sucesso.' });
-                };
-                // Cria o saldo para o usuário e a conta
+                usuarioConta.saldo = Saldo;
+                await usuarioConta.save();
+
+                return res.status(200).json({ message: 'Conta atualizada com sucesso.' });
+            }
+
+            // Verifica se já existe uma conta com o mesmo nome
+            let conta = await ContaBancaria.findOne({ where: { nomeConta } });
+
+            if (!conta) {
+                conta = await ContaBancaria.create({ nomeConta });
+            }
+            // Verifica se já existe um saldo para o usuário e conta específicos
+            const existeSaldo = await UsuarioConta.findOne({
+                where: {
+                    IdConta: conta.IdConta,
+                    IdOrigem
+                }
+            });
+
+            if (existeSaldo) {
+                return res.status(400).json({ error: 'O usuário já possui uma conta cadastrada.' });
+            }
+
+            if (Saldo > 0) {
+                // Cria um saldo para o usuário e a conta
                 await UsuarioConta.create({
                     IdConta: conta.IdConta,
                     IdOrigem,
                     SetorOrigem: 'Usuarios',
-                    saldo: 0.00
+                    saldo: Saldo
                 });
-            }
 
-            usuarioConta.saldo = Saldo;
-            await usuarioConta.save();
+                return res.status(201).json({ message: 'Conta criada e saldo associado ao usuário com sucesso.' });
+            };
 
-            return res.status(200).json({ message: 'Conta atualizada com sucesso.' });
-        }
-
-        // Verifica se já existe uma conta com o mesmo nome
-        let conta = await ContaBancaria.findOne({ where: { nomeConta } });
-
-        if (!conta) {
-            conta = await ContaBancaria.create({ nomeConta });
-        }
-        // Verifica se já existe um saldo para o usuário e conta específicos
-        const existeSaldo = await UsuarioConta.findOne({
-            where: {
-                IdConta: conta.IdConta,
-                IdOrigem
-            }
-        });
-
-        if (existeSaldo) {
-            return res.status(400).json({ error: 'O usuário já possui uma conta cadastrada.' });
-        }
-
-        if (Saldo > 0) {
-            // Cria um saldo para o usuário e a conta
+            // Cria o saldo para o usuário e a conta
             await UsuarioConta.create({
                 IdConta: conta.IdConta,
                 IdOrigem,
                 SetorOrigem: 'Usuarios',
-                saldo: Saldo
+                saldo: 0.00
             });
 
             return res.status(201).json({ message: 'Conta criada e saldo associado ao usuário com sucesso.' });
-        };
 
-        // Cria o saldo para o usuário e a conta
-        await UsuarioConta.create({
-            IdConta: conta.IdConta,
-            IdOrigem,
-            SetorOrigem: 'Usuarios',
-            saldo: 0.00
-        });
-
-        return res.status(201).json({ message: 'Conta criada e saldo associado ao usuário com sucesso.' });
-
-    } catch (error) {
-        console.error('Erro ao criar conta:', error);
-        return res.status(500).json({ error: 'Erro ao criar a conta', detalhes: error });
-    }
-});
+        } catch (error) {
+            console.error('Erro ao criar conta:', error);
+            return res.status(500).json({ error: 'Erro ao criar a conta', detalhes: error });
+        }
+    });
 
 module.exports = router;
